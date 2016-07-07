bd229e77b5e1fa936acc2b3e2ce786fb328879e8
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-07 13:46:16 -0400

    Progress interrupted by thunder so saving commit

--- mightyrestaurant/appmightyrestaurant/models.py
+++ mightyrestaurant/appmightyrestaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+typechoices = (
+        ('owner', 'Owner'),
+        ('cook', 'Cook'),
+        ('server', 'Server'),
+        )
+
+
+class Worker(models.Model):
+    name = models.CharField(max_length=40)
+    workertype = models.CharField(max_length=10, choices=typechoices)
+    hirement = models.DateTimeField(auto_now_add=True)
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=50)
+    foodtype = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
