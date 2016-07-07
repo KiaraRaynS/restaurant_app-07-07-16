21057f300e02fefac7985452a752195b680c7067
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-07 13:15:59 -0400

    Restaurant project initialized - app started and installed - templates directory installed

---
+++ .direnv/python-3.5.1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gingeredmink/Documents/tiy_work/classwork/week_8/day_3/restaurantproject/.direnv/python-3.5.1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mightyrestaurant/appmightyrestaurant/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppmightyrestaurantConfig(AppConfig):
+    name = 'appmightyrestaurant'
